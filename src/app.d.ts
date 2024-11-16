d1db0b4cfa53b41946c929c1b8a9cd0f46542a72
YouHoGeon <me@youhogeon.com>
2024-11-16 19:26:24 +0900

    cloudflare D1 TEST

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,13 +1,26 @@
 // See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
+
+import type { D1Database } from "@cloudflare/workers-types"
+
 declare global {
 	namespace App {
 		// interface Error {}
 		// interface Locals {}
 		// interface PageData {}
 		// interface PageState {}
-		// interface Platform {}
+
+		interface Platform {
+			env: {
+				DB: D1Database;
+			};
+			context: {
+				waitUntil(promise: Promise<any>): void;
+			};
+			caches: CacheStorage & { default: Cache };
+		}
+
 	}
 }
 
-export {};
+export { };
--- src/routes/api/random/+server.ts
+++ src/routes/api/random/+server.ts
@@ -1,17 +1,15 @@
 import { error } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
 
-function sleep(ms: number) {
-	return new Promise(resolve => setTimeout(resolve, ms));
-  } // 함수정의
+export const GET: RequestHandler = async ({ request, platform }) => {
+	if (!platform) {
+		throw new Error("T.T")
+	}
+
+	let result = await platform.env.DB.prepare(
+		"SELECT * FROM users LIMIT 5"
+	).run();
+	return new Response(JSON.stringify(result));
 
 
-export const GET: RequestHandler = async ({ url }) => {
-	// const response = await fetch("http://www.randomnumberapi.com/api/v1.0/random?min=100&max=2000&count=5")
-
-	const value = Math.random() * 100
-
-	await sleep(value)
-
-	return new Response(String(value));
 };
