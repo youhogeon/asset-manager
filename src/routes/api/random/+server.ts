0954f8618848434537c44cfcbb6130e4c024c809
YouHoGeon <me@youhogeon.com>
2024-11-16 16:55:50 +0900

    test cloudflare page max computing time

--- src/routes/api/random/+server.ts
+++ src/routes/api/random/+server.ts
@@ -1,8 +1,17 @@
 import { error } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
 
-export const GET: RequestHandler = async ({ url }) => {
-	const response = await fetch("http://www.randomnumberapi.com/api/v1.0/random?min=100&max=1000&count=5")
+function sleep(ms: number) {
+	return new Promise(resolve => setTimeout(resolve, ms));
+  } // 함수정의
 
-	return response;
+
+export const GET: RequestHandler = async ({ url }) => {
+	// const response = await fetch("http://www.randomnumberapi.com/api/v1.0/random?min=100&max=2000&count=5")
+
+	const value = Math.random() * 100
+
+	await sleep(value)
+
+	return new Response(String(value));
 };
